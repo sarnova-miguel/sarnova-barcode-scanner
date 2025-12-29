dfe774da08cac13a2548abefa403226b7f6d2e50
Miguel Ramos <Miguel.Ramos@sarnova.com>
2025-12-29 17:07:07 -0500

    ui update

--- components/ui/Container.tsx
+++ components/ui/Container.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const Container = ({ children, className }: { children: React.ReactNode; className?: string }) => {
   return (
-    <main className={cn("w-full max-w-7xl mx-auto p-4", className)}>
+    <main className={cn("w-full max-w-7xl mx-auto p-4 md:py-8", className)}>
         {children}
     </main>
   )
